5ff4e5839ec3a3cd3186e553ce43f6c0675e6307
vreddy <vineeth.reddy@ansys.com>
2014-12-13 12:05:49 +0530

    Console Class : Printing output using Concatenation and Place holder syntax

---
+++ SystemConsoleClass/SystemConsoleClass/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SystemConsoleClass
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please Enter a Valid UserName");
+            string userName = Console.ReadLine();
+            //First Way of printing : concatenation
+            Console.WriteLine("Welcome by concatenation" + userName);
+            //Second way of printing : place-holder syntax
+            Console.WriteLine("Welcome by placeholder {0}", userName);
+            Console.WriteLine("Press Any Key to Close");
+            Console.ReadKey();
+        }
+    }
+}
